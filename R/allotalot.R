69bf5c380b2ae6a36079acafcfbabe2b9b4791d2
nteetor <nathanteetor@gmail.com>
2017-01-14 12:24:24 -0500

    updated docs, unpack is no longer internal

--- R/allotalot.R
+++ R/allotalot.R
@@ -1,9 +1,11 @@
 #' Parallel Assignment in R
 #'
-#' allotalot provides tools to perform unpacking and parallel assignment in R.
-#' To get started with allotalot check out the introductory vignette.
+#' allotalot provides an infix function \code{\link{\%<-\%}} to perform
+#' unpacking and parallel assignment in R. To get started with allotalot check
+#' out the introductory vignette, \code{?intro_to_allotalot}.
 #'
 #' @seealso \code{\link{\%<-\%}}
 #'
 #' @docType package
+#' @name allotalot
 '_PACKAGE'
