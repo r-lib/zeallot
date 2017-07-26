81dfd87e5d8db1de397c537bdc32fe56261c7443
nteetor <nathanteetor@gmail.com>
2017-07-26 18:42:18 -0400

    closes #18, ... may now be used to skip values

--- R/pair-off.R
+++ R/pair-off.R
@@ -65,40 +65,72 @@ pair_off <- function(names, values) {
     return()
   }
 
+  #
+  # mismatch between variables and values
+  #
   if (length(names) != length(values)) {
+    #
+    # mismatch could be resolved by destructuring the values, in this case
+    # values must be a single element list
+    #
     if (is_list(values) && length(values) == 1) {
       return(pair_off(names, destructure(car(values))))
     }
 
-    if (!has_collector(names)) {
-      if (length(names) > length(values)) {
-        stop('expecting ', length(names), ' values, but found ', length(values),
-             call. = FALSE)
-      } else {
-        stop('too many values to unpack', call. = FALSE)
-      }
-
-    } else {
-      if (length(names) > length(values)) {
-        stop('expecting at least ', length(names), ' values, but found ',
-             length(values), call. = FALSE)
-      }
+    #
+    # if there is no collector the mismatch is a problem *or* if collector,
+    # but still more variables than values the collector is useless and
+    # mismatch is a problem
+    #
+    if (!has_collector(names) || length(names) > length(values)) {
+      stop(
+        "invalid `%<-%` right-hand side, incorrect number of values",
+        call. = FALSE
+      )
     }
+
+    # if (!has_collector(names)) {
+    #   if (length(names) > length(values)) {
+    #     stop('expecting ', length(names), ' values, but found ', length(values),
+    #          call. = FALSE)
+    #   } else {
+    #     stop('too many values to unpack', call. = FALSE)
+    #   }
+    #
+    # } else {
+    #   if (length(names) > length(values)) {
+    #     stop('expecting at least ', length(names), ' values, but found ',
+    #          length(values), call. = FALSE)
+    #   }
+    # }
   }
 
   if (is_collector(car(names))) {
     collected <- collect(names, values)
     name <- car(names)
 
-    if (name == '...') {
-      stop('invalid collector variable', call. = FALSE)
+    #
+    # skip unnamed collector variable and corresponding values
+    #
+    if (name == "...") {
+      # stop('invalid collector variable', call. = FALSE)
+      return(pair_off(cdr(names), cdr(collected)))
     }
 
-    return(c(pair_off(name, car(collected)), pair_off(cdr(names), cdr(collected))))
+    return(
+      c(pair_off(name, car(collected)), pair_off(cdr(names), cdr(collected)))
+    )
   }
 
+  #
+  # multiple nested variables and nested vector of values same length, but
+  # a nested vector is not unpacked, mismatch
+  #
   if (is_list(names) && !is_list(values)) {
-    stop('expecting ', length(names), ' values, but found 1', call. = FALSE)
+    stop(
+      "invalid `%<-%` right-hand side, incorrect number of values",
+      call. = FALSE
+    )
   }
 
   if (length(names) == 1) {
